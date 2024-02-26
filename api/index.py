3e7a19aaaae01d02bb95c51cc9a57df02d8fea46
gewanbo <gewanbo@gmail.com>
2024-02-26 22:19:36 +0800

    update

--- api/index.py
+++ api/index.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import google.generativeai as genai
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+@app.route('/gen')
+def gen():
+
+    model = genai.GenerativeModel('gemini-pro')
+
+    response = model.generate_content("The opposite of hot is")
+
+    return response.text
 
 @app.route('/about')
 def about():
