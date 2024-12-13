117760ae8b5d15f29b3b128e6bb17f10ec96834d
Wanbo Ge <gewanbo@gmail.com>
2024-12-13 18:56:08 +0800

    Update index.py
    update model name

--- api/index.py
+++ api/index.py
@@ -95,7 +95,7 @@ def trans():
 
     genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-1.5-flash')
 
     response_text = 'This is great!'
     ai_response = ''
